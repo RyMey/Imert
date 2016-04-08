5f7fc76a55e49f0142e486a329c2dea4a817c8e7
Rya Meyvriska <rya.meyvriska@gmail.com>
2016-04-08 17:42:54 +0700

    add xxxhdpi and add about

--- app/src/main/java/id/zelory/imert/MainActivity.java
+++ app/src/main/java/id/zelory/imert/MainActivity.java
@@ -1,23 +1,25 @@
 package id.zelory.imert;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
     Spinner spinner;
     TextView widthInitial, heightInitial;
     ImageView warningWidth, warningHeight;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -36,86 +38,96 @@ public class MainActivity extends AppCompatActivity
         spinner.setAdapter(adapter);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        startActivity(new Intent(this,About.class));
+        return super.onOptionsItemSelected(item);
+    }
+
     /**
      * Display asset size
      *
      * @param view
      */
-    public void displayAssetSize(View view)
-    {
-        if (widthInitial.getText().toString().equals(""))
-        {
+    public void displayAssetSize(View view) {
+        if (widthInitial.getText().toString().equals("")) {
             warningWidth.setVisibility(View.VISIBLE);
             warningHeight.setVisibility(View.INVISIBLE);
-        } else if (heightInitial.getText().toString().equals(""))
-        {
+        } else if (heightInitial.getText().toString().equals("")) {
             warningWidth.setVisibility(View.INVISIBLE);
             warningHeight.setVisibility(View.VISIBLE);
-        } else
-        {
+        } else {
             warningWidth.setVisibility(View.INVISIBLE);
             warningHeight.setVisibility(View.INVISIBLE);
             cekSpinner();
+            View viewKeyboard = this.getCurrentFocus();
+            if (viewKeyboard != null) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(this.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
             ScrollView scrollView = (ScrollView) findViewById(R.id.asset_scroll_view);
             scrollView.setVisibility(View.VISIBLE);
         }
-
     }
 
-    public void cekSpinner()
-    {
-        if(spinner.getSelectedItem().toString().equals("xxhdpi"))
-        {
+    public void cekSpinner() {
+        if (spinner.getSelectedItem().toString().equals("xxxhdpi")) {
+            hitung(4.0);
+        } else if (spinner.getSelectedItem().toString().equals("xxhdpi")) {
             hitung(3.0);
-        }
-        else if(spinner.getSelectedItem().toString().equals("xhdpi"))
-        {
+        } else if (spinner.getSelectedItem().toString().equals("xhdpi")) {
             hitung(2.0);
-        }
-        else if (spinner.getSelectedItem().toString().equals("hdpi"))
-        {
+        } else if (spinner.getSelectedItem().toString().equals("hdpi")) {
             hitung(1.5);
-        }
-        else if(spinner.getSelectedItem().toString().equals("mdpi"))
-        {
+        } else if (spinner.getSelectedItem().toString().equals("mdpi")) {
             hitung(1.0);
-        }
-        else if(spinner.getSelectedItem().toString().equals("ldpi"))
-        {
+        } else if (spinner.getSelectedItem().toString().equals("ldpi")) {
             hitung(0.75);
         }
     }
-    public void hitung(double x)
-    {
-        TextView widthXxhdpi,widthXhdpi,widthHdpi,widthMdpi,widthLdpi,heightXxhdpi,heightXhdpi,heightHdpi,heightMdpi,heightLdpi;
+
+    public void hitung(double x) {
+        TextView widthXxxhdpi, widthXxhdpi, widthXhdpi, widthHdpi, widthMdpi, widthLdpi, heightXxxhdpi, heightXxhdpi, heightXhdpi, heightHdpi, heightMdpi, heightLdpi;
         double w0 = Double.parseDouble(widthInitial.getText().toString());
         double h0 = Double.parseDouble(heightInitial.getText().toString());
 
         //find view by id and set text
+        widthXxxhdpi = (TextView) findViewById(R.id.width_xxxhdpi);
+        widthXxxhdpi.setText("width\t:\t" + (w0 / x) * 4);
+        heightXxxhdpi = (TextView) findViewById(R.id.height_xxxhdpi);
+        heightXxxhdpi.setText("height\t:\t" + (h0 / x) * 4);
+
         widthXxhdpi = (TextView) findViewById(R.id.width_xxhdpi);
-        widthXxhdpi.setText("width\t:\t"+ (w0/x)*3);
+        widthXxhdpi.setText("width\t:\t" + (w0 / x) * 3);
         heightXxhdpi = (TextView) findViewById(R.id.height_xxhdpi);
-        heightXxhdpi.setText("height\t:\t"+ (h0/x)*3);
+        heightXxhdpi.setText("height\t:\t" + (h0 / x) * 3);
 
         widthXhdpi = (TextView) findViewById(R.id.width_xhdpi);
-        widthXhdpi.setText("width\t:\t"+ (w0/x)*2);
+        widthXhdpi.setText("width\t:\t" + (w0 / x) * 2);
         heightXhdpi = (TextView) findViewById(R.id.height_xhdpi);
-        heightXhdpi.setText("height\t:\t"+ (h0/x)*2);
+        heightXhdpi.setText("height\t:\t" + (h0 / x) * 2);
 
         widthHdpi = (TextView) findViewById(R.id.width_hdpi);
-        widthHdpi.setText("width\t:\t"+ (w0/x)*1.5);
+        widthHdpi.setText("width\t:\t" + (w0 / x) * 1.5);
         heightHdpi = (TextView) findViewById(R.id.height_hdpi);
-        heightHdpi.setText("height\t:\t"+ (h0/x)*1.5);
+        heightHdpi.setText("height\t:\t" + (h0 / x) * 1.5);
 
         widthMdpi = (TextView) findViewById(R.id.width_mdpi);
-        widthMdpi.setText("width\t:\t"+ (w0/x)*1);
+        widthMdpi.setText("width\t:\t" + (w0 / x) * 1);
         heightMdpi = (TextView) findViewById(R.id.height_mdpi);
-        heightMdpi.setText("height\t:\t"+ (h0/x)*1);
+        heightMdpi.setText("height\t:\t" + (h0 / x) * 1);
 
         widthLdpi = (TextView) findViewById(R.id.width_ldpi);
-        widthLdpi.setText("width\t:\t"+ (w0/x)*0.75);
+        widthLdpi.setText("width\t:\t" + (w0 / x) * 0.75);
         heightLdpi = (TextView) findViewById(R.id.height_ldpi);
-        heightLdpi.setText("height\t:\t"+ (h0/x)*0.75);
+        heightLdpi.setText("height\t:\t" + (h0 / x) * 0.75);
 
     }
 }
